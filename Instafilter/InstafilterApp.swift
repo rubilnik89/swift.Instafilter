129ae8a47ad843695f013da83cc3ada095c03f60
Roman Zherebko <romanzherebko@Mac-mini-Roman.netis>
2022-06-21 18:45:13 +0300

    app

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Roman Zherebko on 21.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
